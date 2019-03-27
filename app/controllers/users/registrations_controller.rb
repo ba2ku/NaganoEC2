e48c97ccca60d33b1215bf17c71126e36ee0c1ea
babamasaki <0508baba@gmail.com>
2019-03-27 07:33:53 +0000

    top完了しました

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,11 +12,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  def create
-    @user = User.new(user_params)
-    @user.save!
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit
